4ed48932c83a9735c7490802934e1ddffd1aa055
varHarrie <varharrie@gmail.com>
2019-03-11 20:04:18 +0800

    Improve useFormStore

--- packages/form/src/useFormStore.ts
+++ packages/form/src/useFormStore.ts
@@ -1,11 +1,10 @@
 import * as React from 'react'
 
-import { FormRules } from './FormStore'
-import { FormStore } from '.'
+import FormStore, { FormRules } from './FormStore'
 
 export default function useFormStore<T extends Object = any> (
   values: Partial<T> = {},
   rules: FormRules = {}
 ) {
-  return React.useState(new FormStore(values, rules))[0]
+  return React.useMemo(() => new FormStore(values, rules), [values, rules])
 }
